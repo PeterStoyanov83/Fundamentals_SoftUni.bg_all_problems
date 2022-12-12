05e7833d6188dc3841afa29f2ddd5412403f3883
petar.stoyanov <peterstoyanov83@gmail.com>
2022-12-12 10:37:45 +0100

    .

---
+++ 03 - Lists Basics/Exercises/01- Invert Values.py
@@ -0,0 +1,14 @@
+string_to_list_input = input().split(" ")
+inverted  = []
+for string in string_to_list_input:
+    string = int(string * -1)
+    inverted.append(str(string))
+print(inverted)
+
+
+"""
+1. Invert Values
+
+Write a program that receives a single string containing positive and negative numbers separated by a single space.
+Print a list containing the opposite of each number.
+"""
