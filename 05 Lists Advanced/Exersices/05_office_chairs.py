0c177c39fa17816c61b062f108e4b522cfc78e6b
petar.stoyanov <peterstoyanov83@gmail.com>
2022-12-16 10:04:54 +0100

    .

--- 05 Lists Advanced/Exersices/05_office_chairs.py
+++ 05 Lists Advanced/Exersices/05_office_chairs.py
@@ -1,15 +1,15 @@
 def check_chairs(n):
-    total_free_chairs, needed_chairs = 0, 0
+    total_free_chairs = 0
+    needed_chairs = 0
     for number_of_room in range(1, n + 1):
         free_chairs, visitors = input().split()
         difference = len(free_chairs) - int(visitors)
         if difference >= 0:
             total_free_chairs += difference
-
         else:
             needed_chairs += abs(difference)
             print(f"{abs(difference)} more chairs needed in room {number_of_room}")
-        return total_free_chairs, needed_chairs
+    return total_free_chairs, needed_chairs
 
 
 number_of_rooms = int(input())
