42b1cd856283595e30a407ef88b2c0d25420b80e
petar.stoyanov <peterstoyanov83@gmail.com>
2022-12-22 13:30:42 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	06 Mid Exam Preparation/Numbers/numbers.py

--- 07 Objects and Classes/Exersices/08_vehicle.py
+++ 07 Objects and Classes/Exersices/08_vehicle.py
@@ -1,26 +1,30 @@
 class Vehicle:
-    def __init__(self, type, model, price, owner =None ):
+    def __init__(self, type, model, price, owner=None):
         self.type = type
         self.model = model
         self.price = price
         self.owner = owner
 
-    def buy(self,money: int, owner: str):
+    def buy(self, money: int, owner: str):
         self.money = money
-        change = abs(self.money - self.price)
-        if self.owner is not None:
-            if money >= self.price and self.owner == None:
-                self.owner = owner
 
-                return f"Successfully bought a {self.type}. Change: {change}"
-            elif money < self.price:
-                return "Sorry, not enough money"
+        if self.owner is not None and money >= self.price:
+            self.owner = owner
+            change = self.money - self.price
+            return f"Successfully bought a {self.type}. Change: {change}"
+        elif money < self.price:
+            return "Sorry, not enough money"
+        else:
+            return "Car already sold"
+
+    def sell(self):
+        if self.owner is not None:
+            self.owner = None
         else:
             return "Vehicle has no owner"
 
-    def sell(self):
-        pass
-
     def __repr__(self):
-        return  f"{model} {type} is owned by: {owner}".
-        return  f"{model} {type} is on sale: {price}"
+        if self.owner:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        else:
+            return f"{self.model} {self.type} is on sale: {self.price}"
