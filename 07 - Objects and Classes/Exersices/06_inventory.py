8fbc1fd981e9b618e6e503766f27ace58236d2c5
petar.stoyanov <peterstoyanov83@gmail.com>
2022-12-22 13:30:42 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	06 Mid Exam Preparation/Numbers/numbers.py

---
+++ 07 - Objects and Classes/Exersices/06_inventory.py
@@ -0,0 +1,28 @@
+class Inventory:
+    __capacity = 0
+    items = []
+
+    def __init__(self, capacity: int):
+        Inventory.__capacity = capacity
+
+    def add_item(self, item: str):
+        if len(Inventory.items) < Inventory.__capacity:
+            Inventory.items.append(item)
+
+        else:
+            return "not enough room in the inventory"
+
+    def get_capacity(self):
+        return Inventory.__capacity
+
+    def __repr__(self):
+        return f"Items: {', '.join(Inventory.items)}.\n" \
+               f"Capacity left: {Inventory.__capacity - len(Inventory.items)}"
+
+
+inventory = Inventory(2)
+inventory.add_item("potion")
+inventory.add_item("sword")
+print(inventory.add_item("bottle"))
+print(inventory.get_capacity())
+print(inventory)
