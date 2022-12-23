37f7bc502f978dc65797aa8da25487b10e362ae7
petar.stoyanov <peterstoyanov83@gmail.com>
2022-12-23 13:10:37 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	06 Mid Exam Preparation/Numbers/numbers.py

---
+++ 08 - Dictionaries/Exersices/03_capitals.py
@@ -0,0 +1,7 @@
+countries = input().split(", ")
+capitals = input().split(", ")
+
+# result = {countries[index]: capitals[index] for index in range(len(countries))}
+result = dict(zip(countries, capitals))             #zip method
+for country, capital in result.items():
+    print(f"{country} -> {capital}")
