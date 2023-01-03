54e563c01e88c63dd350b0597ac461dcf8ca6efa
petar.stoyanov <peterstoyanov83@gmail.com>
2023-01-03 13:22:06 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	06 Mid Exam Preparation/Numbers/numbers.py

--- 09 - Text Processing/Exersices/03_extract_file.py
+++ 09 - Text Processing/Exersices/03_extract_file.py
@@ -1,1 +1,7 @@
 
+filename_with_extensions = input().split("\\")
+filename, extension = filename_with_extensions[-1].split(".")
+
+print(f"File name: {filename}")
+print(f'File extension: {extension}')
+
