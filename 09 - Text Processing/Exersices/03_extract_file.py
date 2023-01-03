b5a91fc8efcac92cb7759c398bc40a76b92ea671
petar.stoyanov <peterstoyanov83@gmail.com>
2023-01-03 13:30:24 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	06 Mid Exam Preparation/Numbers/numbers.py

--- 09 - Text Processing/Exersices/03_extract_file.py
+++ 09 - Text Processing/Exersices/03_extract_file.py
@@ -5,3 +5,20 @@ filename, extension = filename_with_extensions[-1].split(".")
 print(f"File name: {filename}")
 print(f'File extension: {extension}')
 
+
+
+# _____________________
+
+import re
+
+# Read the input filename with extensions
+filename_with_extensions = input()
+
+# Use a regular expression to extract the filename and extension from the input
+match = re.match(r".*\\(.*)\.(.*)", filename_with_extensions)
+filename = match.group(1)
+extension = match.group(2)
+
+# Print the filename and extension
+print(f"File name: {filename}")
+print(f'File extension: {extension}')
