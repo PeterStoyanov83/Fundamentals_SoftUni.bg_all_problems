d50667c7f1a7735b923b36173ac2cb724bfda5ea
petar.stoyanov <peterstoyanov83@gmail.com>
2023-01-03 14:42:07 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	06 Mid Exam Preparation/Numbers/numbers.py

--- 09 - Text Processing/Exersices/08_letters_change_numbers.py
+++ 09 - Text Processing/Exersices/08_letters_change_numbers.py
@@ -1,28 +1,19 @@
-def process_string(string):
-    # Extract the number and the letters from the string
-    number = int(string[0])
-    first_letter = string[1]
-    second_letter = string[2]
-
-    # Perform the first operation based on the case of the first letter
+data = input().split()
+total_sum = 0
+for current_string in data:
+    current_number = int(current_string[1:len(current_string)-1])
+    first_letter = current_string[0]
+    last_letter = current_string[-1]
     if first_letter.isupper():
-        number /= ord(first_letter) - 64
-    else:
-        number *= ord(first_letter) - 96
-
-    # Perform the second operation based on the case of the second letter
-    if second_letter.isupper():
-        number -= ord(second_letter) - 64
-    else:
-        number += ord(second_letter) - 96
-
-    return number
-
-# Read the input strings
-strings = input("Enter the strings: ").split()
-
-# Process the strings and sum the results
-result = sum(process_string(string) for string in strings)
-
-# Print the result
-print(f"{result:.2f}")
+        first_letter_position = ord(first_letter) - 64
+        total_sum += current_number / first_letter_position
+    elif first_letter.islower():
+        first_letter_position = ord(first_letter) - 96
+        total_sum += current_number * first_letter_position
+    if last_letter.isupper():
+        last_letter_position = ord(last_letter) - 64
+        total_sum -= last_letter_position
+    elif last_letter.islower():
+        last_letter_position = ord(last_letter) - 96
+        total_sum += last_letter_position
+print(f"{total_sum:.2f}")
