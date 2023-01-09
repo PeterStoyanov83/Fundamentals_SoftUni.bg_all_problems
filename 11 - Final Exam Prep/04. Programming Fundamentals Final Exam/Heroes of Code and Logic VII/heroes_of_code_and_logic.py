e0b7f2ec5d2b70e8c053dcf53921c235c06cb7c8
petar.stoyanov <peterstoyanov83@gmail.com>
2023-01-09 16:01:32 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	06 Mid Exam Preparation/Numbers/numbers.py

--- 11 - Final Exam Prep/04. Programming Fundamentals Final Exam/Heroes of Code and Logic VII/heroes_of_code_and_logic.py
+++ 11 - Final Exam Prep/04. Programming Fundamentals Final Exam/Heroes of Code and Logic VII/heroes_of_code_and_logic.py
@@ -1,9 +1,48 @@
+def cast_spell(name, mana_points, spell_n, heroes_dict):
+    if heroes_dict[name]["MP"] >= mana_points:
+        heroes_dict[name]['MP'] -= mp_needed
+        print(f"{name} has successfully cast {spell_n} and now has {heroes_dict[name]['MP']} MP!")
+    else:
+        print(f"{name} does not have enough MP to cast {spell_n}!")
+    return heroes_dict
+
+
+def take_damage(hero_name, damage, attacker, heroes_dict):
+    if heroes_dict[hero_name]["HP"] - damage <= 0:
+        del heroes_dict[hero_name]
+        print(f"{hero_name} has been killed by {attacker}!")
+    else:
+        heroes_dict[hero_name]["HP"] -= damage
+        print(f"{hero_name} was hit for {damage} HP by {attacker} and now has {heroes_dict[hero_name]['HP']} HP left!")
+    return heroes
+
+
+def recharge(name, amount_to_charge, heroes_dict):
+    if heroes_dict[name]["MP"] + amount_to_charge > 200:
+        print(f"{name} recharged for {200 - heroes_dict[name]['MP']} MP!")
+        heroes_dict[name]["MP"] = 200
+    else:
+        heroes_dict[name]["MP"] += amount_to_charge
+        print(f"{name} recharged for {amount_to_charge} MP!")
+    return heroes_dict
+
+
+def heal(hero_name, amount, heroes_dict):
+    if heroes_dict[hero_name]['HP'] + amount >= 100:
+        hp_gained = 100 - heroes_dict[hero_name]['HP']
+    elif heroes_dict[hero_name]['HP'] + amount < 100:
+        hp_gained = amount
+    heroes_dict[hero_name]['HP'] += hp_gained
+    print(f"{hero_name} healed for {hp_gained} HP!")
+    return heroes
+
+
 n = int(input())
 
 heroes = {}
 
 for _ in range(n):
-    hero_name, hp, mp = input().split()
+    hero_name, hp, mp = input().split(" ")
     heroes[hero_name] = {"HP": int(hp), "MP": int(mp)}
     """this creates a dictionary where each key is a hero name and the value is a dictionary 
     containing the hero's HP and MP. The inner dictionary is referred to as a nested dictionary."""
@@ -15,43 +54,22 @@ while data != "End":
     if split_data[0] == "CastSpell":
         hero_name, mp_needed, spell_name = split_data[1:]
         mp_needed = int(mp_needed)
-        if heroes[hero_name]["MP"] >= mp_needed:
-            heroes[hero_name]['MP'] -= mp_needed
-            print(f"{hero_name} has successfully cast {spell_name} and now has {heroes[hero_name]['MP']} MP!")
-        else:
-            print(f"{hero_name} does not have enough MP to cast {spell_name}!")
+        heroes = cast_spell(hero_name, mp_needed, spell_name, heroes)
 
     elif split_data[0] == "TakeDamage":
         hero_name, damage, attacker = split_data[1:]
         damage = int(damage)
-
-        if heroes[hero_name]["HP"] - damage <= 0:
-            del heroes[hero_name]
-            print(f"{hero_name} has been killed by {attacker}!")
-        else:
-            heroes[hero_name]["HP"] -= damage
-            print(f"{hero_name} was hit for {damage} HP by {attacker} and now has {heroes[hero_name]['HP']} HP left!")
+        heroes = take_damage(hero_name, damage, attacker, heroes)
 
     elif split_data[0] == "Recharge":
         hero_name, amount = split_data[1:]
         amount = int(amount)
-        if heroes[hero_name]['MP'] + amount >= 200:
-            mp_gained = 200 - heroes[hero_name]['MP']
-        elif heroes[hero_name]['MP'] + amount < 200:
-            mp_gained = amount
-        heroes[hero_name]['MP'] += mp_gained
-        print(f"{hero_name} recharged for {mp_gained} MP!")
+        heroes = recharge(hero_name, amount, heroes)
 
     elif split_data[0] == "Heal":
         hero_name, amount = split_data[1:]
         amount = int(amount)
-
-        if heroes[hero_name]['HP'] + amount >= 100:
-            hp_gained = 100 - heroes[hero_name]['HP']
-        elif heroes[hero_name]['HP'] + amount < 100:
-            hp_gained = amount
-        heroes[hero_name]['HP'] += hp_gained
-        print(f"{hero_name} healed for {hp_gained} HP!")
+        heroes = heal(hero_name, amount, heroes)
 
     data = input()
 
@@ -59,39 +77,3 @@ for name, values in heroes.items():
     print(name)
     print(f"  HP: {values['HP']}")
     print(f"  MP: {values['MP']}")
-
-# _________________solution by open AI ______________________
-# n = int(input())
-#
-# heroes = {}
-#
-# for i in range(n):
-#     hero_info = input().split()
-#     name = hero_info[0]
-#     hp = int(hero_info[1])
-#     mp = int(hero_info[2])
-#     heroes[name] = {'HP': hp, 'MP': mp}
-#
-# while True:
-#     command = input().split(" - ")
-#     if command[0] == "End":
-#         break
-#     elif command[0] == "CastSpell":
-#         hero = command[1]
-#         mp_needed = int(command[2])
-#         spell = command[3]
-#         if heroes[hero]['MP'] < mp_needed:
-#             print(f"{hero} does not have enough MP to cast {spell}!")
-#         else:
-#             heroes[hero]['MP'] -= mp_needed
-#             print(f"{hero} has successfully cast {spell} and now has {heroes[hero]['MP']} MP!")
-#     elif command[0] == "TakeDamage":
-#         hero = command[1]
-#         damage = int(command[2])
-#         attacker = command[3]
-#         heroes[hero]['HP'] -= damage
-#         if heroes[hero]['HP'] <= 0:
-#             print(f"{hero} has been killed by {attacker}!")
-#             heroes.pop(hero)
-#         else:
-#             print(f"{hero} was hit for {damage}")
