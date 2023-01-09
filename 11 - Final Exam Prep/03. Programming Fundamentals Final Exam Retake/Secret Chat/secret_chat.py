64f2df3b018753276aff90eb9291af4039ee9494
petar.stoyanov <peterstoyanov83@gmail.com>
2023-01-09 10:24:31 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	06 Mid Exam Preparation/Numbers/numbers.py

--- 11 - Final Exam Prep/03. Programming Fundamentals Final Exam Retake/Secret Chat/secret_chat.py
+++ 11 - Final Exam Prep/03. Programming Fundamentals Final Exam Retake/Secret Chat/secret_chat.py
@@ -1,73 +1,30 @@
-def insert_space(index, data):
-    # slicing and adding a space on the current index
-    data = data[:index] + " " + data[index:]
-    print(data)
-
-
-def reverse(substring, data):
-
-    if substring in data:
-        data = data.replace(substring, "", 1)  # replace 1st match of the substring with nothing ("")
-        data = data + substring[::-1]  # adding the reversed substring to the end of the data string
-        print(data)
-    else:
-        print("error")
-
-
-def change_all(substring, replacement, data):
-    data = data.replace(substring, replacement)
-    print(data)
-
-
-data = str(input())
-
+data = input()
 
 while True:
+
     command = input().split(':|:')
-    if command == "Reveal":
+    current_command = command[0]
+
+    if current_command == 'Reveal':
         print(f"You have a new text message: {data}")
         break
 
-    elif command[0] == "InsertSpace":
+    elif current_command == 'InsertSpace':
         index = int(command[1])
-        insert_space(index, data)
+        data = data[:index] + ' ' + data[index:]
+        print(data)
 
-    elif command[0] == "Reverse":
+    elif current_command == 'Reverse':
         substring = command[1]
-        reverse(substring, data)
+        if substring in data:
+            data = data.replace(substring, '', 1)
+            data = data + substring[::-1]
+            print(data)
+        else:
+            print('error')
 
-    elif command[0] == "ChangeAll":
+    elif current_command == 'ChangeAll':
         substring, replacement = command[1], command[2]
-        change_all(substring, replacement, data)
+        data = data.replace(substring, replacement)
+        print(data)
 
-
-# ______________________________________________________
-# data = input()
-#
-# while True:
-#
-#     command = input().split(':|:')
-#     current_command = command[0]
-#
-#     if current_command == 'Reveal':
-#         print(f"You have a new text message: {data}")
-#         break
-#
-#     elif current_command == 'InsertSpace':
-#         index = int(command[1])
-#         data = data[:index] + ' ' + data[index:]
-#         print(data)
-#
-#     elif current_command == 'Reverse':
-#         substring = command[1]
-#         if substring in data:
-#             data = data.replace(substring, '', 1)
-#             data = data + substring[::-1]
-#             print(data)
-#         else:
-#             print('error')
-#
-#     elif current_command == 'ChangeAll':
-#         substring, replacement = command[1], command[2]
-#         data = data.replace(substring, replacement)
-#         print(data)
--- 11 - Final Exam Prep/04. Programming Fundamentals Final Exam/Fancy Barcodes/fancy_barcodes.py
+++ 11 - Final Exam Prep/04. Programming Fundamentals Final Exam/Fancy Barcodes/fancy_barcodes.py
@@ -4,6 +4,12 @@ number_of_barcode = int(input())
 for _ in range(number_of_barcode):
     data = input()
     pattern = r'(\@\#+)([A-Z][A-Za-z0-9]{4,}[A-Z])(\@\#+)'
+
+    """"@+#+[A-Z][A-Za-z0-9]{4,}[A-Z]#+@+"
+    This RegEx will match a string that starts with "@" followed by one or more "#" symbols, then a capital letter, 
+    followed by 4 or more characters that are either letters (upper or lowercase) or digits, followed by a capital 
+    letter, followed by one or more "#" symbols, and ending with "@"."""
+
     result = re.match(pattern, data)
 
     if not result:
@@ -15,3 +21,6 @@ for _ in range(number_of_barcode):
             print("Product group: 00")
         else:
             print(f"Product group: {''.join(extract_numbers)}")
+
+
+
