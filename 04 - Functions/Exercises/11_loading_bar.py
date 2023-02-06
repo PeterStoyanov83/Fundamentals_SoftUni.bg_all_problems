0c378db2be77a2f87d9904a53fb38355b83f5da1
petar.stoyanov <peterstoyanov83@gmail.com>
2023-02-06 16:02:06 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

--- 04 - Functions/Exercises/11_loading_bar.py
+++ 04 - Functions/Exercises/11_loading_bar.py
@@ -1,4 +1,4 @@
-def check_loadinf_prog(a):
+def check_loading_prog(a):
     if a == 100:
         return "100% Complete!\n[%%%%%%%%%%]"
     return f"{a}% [{'%' * (a // 10)}{'.' * (10 - (a // 10))}]\nStill loading..."
@@ -6,4 +6,4 @@ def check_loadinf_prog(a):
 
 bar_value = int(input())
 
-print(check_loadinf_prog(bar_value))
+print(check_loading_prog(bar_value))
