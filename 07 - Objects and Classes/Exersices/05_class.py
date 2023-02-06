f6ecb2ef4a38a1729cc017010d7e85d6917a8c9b
petar.stoyanov <peterstoyanov83@gmail.com>
2023-02-06 16:28:28 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

--- 07 - Objects and Classes/Exersices/05_class.py
+++ 07 - Objects and Classes/Exersices/05_class.py
@@ -1,18 +1,18 @@
-class Inventory:
-    __capacity = 0
-    items = []
+class Class:
+    __students_count = 22
 
-    def __init__(self, capacity: int):
-        Inventory.__capacity = capacity
+    def __init__(self, name: str):
+        self.name = name
+        self.students = []
+        self.grades = []
 
-    def add_item(self, item: str):
-        if len(Inventory.items) < Inventory.__capacity:
-            Inventory.items.append(item)
+    def add_student(self, name: str, grade: float):
+        if len(self.students) < Class.__students_count:
+            self.students.append(name)
+            self.grades.append(grade)
 
-        return "not enough room in the inventory"
-
-    def get_capacity(self):
-        return Inventory.__capacity
+    def get_average_grade(self):
+        return round(sum(self.grades) / len(self.grades), 2)
 
     def __repr__(self):
-        return f"Items: {', '.join(Inventory.items)}.\nCapacity left: {Inventory.__capacity - len(Inventory.items)}"
+        return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {self.get_average_grade()}"
